9b122ba9249524169bb02fec6fbdf9a60f6430b6
Mathleonard <victor.castaneda@ciencias.unam.mx>
2023-02-21 23:40:05 -0600

    We made the first case: a<b
    Co-authored-by: Ian P.C. <Ian326@users.noreply.github.com>

---
+++ Proyecto 1/mcdmcm.py
@@ -0,0 +1,40 @@
+#librería para poder utilizar funciones matemáticas
+import math
+#en la primera iteración, "b" es el número más grande, y "r" el número más pequeño
+def euclides(b, r):
+    cociente_e = math.floor(b/r)
+    residuo_e = b%r
+    print(f"{b}={r}({cociente_e})+{residuo_e}")
+    #!= significa "no es igual a"
+    if residuo_e != 0:
+        #se repite el proceso con la pareja (r,residuo_e)
+        euclides(r,residuo_e)
+    else:
+        print(f"El máximo común divisor es {r}.")
+#función inicial: elegir un número (que serían dos números)
+def select():
+    #Guarda la variable como "first_number". Si el usuario no ingresa un número,
+    #la app finaliza con un error
+    first_number = int(input("Escribe tu primer número: "))
+    second_number = int(input("Escribe tu segundo número: "))
+    #f hace que se puedan poner las variables en el texto con unas llaves
+    print(f"Escogiste la pareja ({first_number}, {second_number}).")
+    if first_number<second_number:
+        print(f"{first_number} es menor que {second_number}.")
+        #la función math.floor es la función piso
+        #cociente = math.floor(second_number/first_number)
+        #residuo = second_number%first_number
+        euclides(second_number, first_number)
+    #elif es tener varias condiciones hasta que se escriba un else (la última posibilidad)
+    elif second_number<first_number:
+        print(f"{second_number} es menor que {first_number}.")
+
+    #la última condición se pone con "else"
+    else:
+        print(f"{first_number} es igual que {second_number}.")
+        print("El máximo común divisor es 1.")
+        #Línea de comando donde se indica la ecuación para que 1 se exprese como
+        #combinación lineal de los números iguales
+        print("El mínimo común múltiplo es 3.")
+
+select()
