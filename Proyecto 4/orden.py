e86a59fa4449fca50506a4251a9e5470e0583ad1
Mathleonard <victor.castaneda@ciencias.unam.mx>
2023-05-07 00:21:58 -0600

    Final review :)
    Co-authored-by: Ian P.C. <Ian326@users.noreply.github.com>

--- Proyecto 4/orden.py
+++ Proyecto 4/orden.py
@@ -1,22 +1,88 @@
+#Definición de la función divisores
+def divisores(n):
+    #Se crea la lista vacía de divisores
+    listaDivisores = []
+    #Asignamos la iteración a partir de 1
+    iteracion_numero = 1
+    #Cada uno de los números deberá cumplir la condición
+    while iteracion_numero <= n/2:
+        if n%iteracion_numero == 0:
+            #'.append()' agrega ese elemento al final de la lista
+            listaDivisores.append(iteracion_numero)
+        iteracion_numero += 1
+        #Como un 'else' no afectaría a la lista, no se agrega
+    #print(f"Sus divisores son {listaDivisores}.")
+    #Regresa la lista de divisores a donde se llamo la función
+    listaDivisores.append(n)
+    return listaDivisores
+
+def algoritmo_div(n, a):
+
+    #Cuando nada más hay un '=', el programa guarda el valor del lado derecho
+    # a las variables del lado izquierdo.
+    #El uso de '%' es para obtener el residuo del cociente: el módulo
+
+    residuo_ad = n%a
+
+    #'f' permite ingresar variables dentro de un texto común (string)
+    #Las variables se ponen entre llaves, cuando estamos en texto común
+
+    #La función 'if' empieza como un 'si (condición)'
+    #'!=' significa 'no es igual a'
+    if residuo_ad != 0:
+        return algoritmo_div(a,residuo_ad)
+
+    #'else' es el último caso del condicional, y se usa cuando no se cumple
+
+    else:
+        s = abs(a)
+        #'return <>' regresa el valor a donde fue llamada la función
+        return s
+
 #Definición de la función phi
-def phi(n):
+def phi(m):
+
     #Definimos la lista para los primos relativos
     listaPrimosRelativos = []
+
     #La iteración empezará a partir de 1
     iteracion_numero = 1
-    #Todos los números menores o iguales a n cumplirán una función
-    while iteracion_numero <= n:
-        mcd = algoritmo_div(n,iteracion_numero)
+
+    #Todos los números menores o iguales a m cumplirán una función
+    while iteracion_numero <= m:
+
+        mcd = algoritmo_div(m,iteracion_numero)
+
         if mcd == 1:
+
             listaPrimosRelativos.append(iteracion_numero)
         #Una vez evaluado, pasamos al siguiente número (Le añadimos uno a la variable)    
+
         iteracion_numero += 1
+
     numeroPrimosRelativos = len(listaPrimosRelativos)
-    #print(f"La función φ({n})={numeroPrimosRelativos}.")
-    #Si se desea ver la lista de los primos relativos, quitar el '#' de a continuación
-    #print(f"Los elementos de la función φ({n})={listaPrimosRelativos}.")
-  
+
+    return numeroPrimosRelativos
+
+def ordm(a, m, lista_divisores):
+
+    #Definimos la lista para los órdenes posibles
+    ordenPosible = []
+
+    for divisor in lista_divisores:
+
+        e = (a**(divisor))%m
+
+        if e == 1:
+
+            ordenPosible.append(divisor)
+
+    o = min(ordenPosible)
+
+    return o
+
 def elegir():
+
     #Si el usuario no ingresa un número, la app finaliza con un error.
     #'input()' escribe un mensaje en la terminal y recibe el texto que
     # el usuario ingrese.
@@ -24,27 +90,42 @@ def elegir():
     # es un entero positivo (se combina con la función if para la
     # condición '>0')  
     valid_input = False
-    while valid_input == False:
+
+    while not valid_input:
+
         m = int(input("Escribe tu módulo: "))
-    if m > 0:
-        valid_input = True
-        print (f"Escogiste el módulo {m}.")
-    else:
-        print ("Escogiste un número inválido.")
+
+        if m > 0:
+
+            valid_input = True
+            print (f"Escogiste el módulo {m}.")
+
+        else:
+
+            print ("Escogiste un número inválido.")
+
     valid_input_2 = False
-    while valid_input_2 == False:
+
+    while not valid_input_2:
+
         a = int(input(f"Escribe tu número al que le sacaremos el orden módulo {m}: "))
-    if a > 0:
-        valid_input_2 = True
-        print (f"Escogiste el número {a}.")
-    #Cuando el número sea valido, llama a las funciones respectivas
+
+        if a > 0:
+
+            valid_input_2 = True
+            print (f"Escogiste el número {a}.")
+
+        else:
+
+            print ("Escogiste un número inválido.")
+
+    #Cuando los números sean válidos, llamamos a las funciones respectivas
         #Llamamos 'listaDivisores' a la lista que nos arroja la función
         # divisores, y la insertamos en tau y sigma
-    #listaDivisores = divisores(n)
-    #tau(n,listaDivisores)
-    #sigma(n,listaDivisores)
-    #phi(n)
-    else:
-        print ("Escogiste un número inválido.")
+    phi_m = phi(m)
+    lista_divisores = divisores(phi_m)
+    o = ordm(a, m, lista_divisores)
+    print(f"El orden de {a} módulo {m} es {o}.")
+
 #Finalmente, llamamos a la funcion inicial del programa, para cuando se ejecute.
 elegir()
